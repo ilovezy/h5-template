a6725057759da2f92e94305c29b508bd3b409d68
fhc <1518265554@qq.com>
2018-11-07 10:26:40 +0800

    a

--- src/main.js
+++ src/main.js
@@ -46,7 +46,7 @@ router.beforeEach((to, from, next) => {
   store.commit('updateLoadingStatus', {isLoading: true})
 
   //动态修改标题
-  document.title = '闪电借条 - ' + to.meta.title
+  document.title = 'h5-template - ' + to.meta.title
   //路由切换动画
   let routeLength = store.state.routeChain.length;
 
